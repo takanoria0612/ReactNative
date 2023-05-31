f08b281eabd8d405ad6472363803bb0400fa249c
takanoria0612 <takanorihiraki06@gmail.com>
2023-06-01 00:03:09 +0900

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState, useEffect } from "react";
+import { Button, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const [newCount, setNewCount] = useState(0);
+  useEffect(() => {
+    console.log("I'm rendereing");
+}, [count])
+  const add = () => {
+    setCount(count + 1);
+  };
+
+  const minus = () => {
+    setNewCount(newCount - 1);
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider style={styles.container}>
+      <SafeAreaView>
+        <Button title="Add" onPress={add} />
+        <Text style={styles.label}>{ count }</Text>
+
+        <Button title="Minus" onPress={minus} />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
